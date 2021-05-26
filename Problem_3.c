5ce32c6913014b7c0f59398b4bf6a921539776dc
JAHNAVIGN <>
2021-05-26 12:00:25 +0000

    learnt to check odd and even integer

--- Problem_3.c
+++ Problem_3.c
@@ -2,14 +2,14 @@
 
 #include <stdio.h>
 int main() {
-    int n;
+    int number;
     printf("Enter an integer: ");
-    scanf("%d", &n);
+    scanf("%d", &number);
 
-    if(n%2==0)
-    printf("even");
+    if(number%2==0)
+    printf("%d is Even\n",number);
     else
-    printf("odd");
+    printf("%d is Odd\n",number);
     
    
     return 0;
